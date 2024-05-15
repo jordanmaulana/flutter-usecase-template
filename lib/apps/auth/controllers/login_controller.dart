a14fbe7c36c1111a47f8fab9e6c681c070e0c8ea
Jordan Maulana <jordanmaulana26@gmail.com>
2024-05-15 21:10:38 +0700

    get profile after login

--- lib/apps/auth/controllers/login_controller.dart
+++ lib/apps/auth/controllers/login_controller.dart
@@ -2,9 +2,11 @@ import 'package:get/get.dart';
 
 import '../../../components/popup.dart';
 import '../../../configs/route_name.dart';
+import '../../profile/controllers/profile_controller.dart';
 import '../usecases/login_usecase.dart';
 
 class LoginController extends GetxController {
+  final ProfileController _profileController = Get.find();
   final LoginUsecase _loginUsecase = Get.find();
 
   RxBool obscureText = true.obs;
@@ -16,6 +18,7 @@ class LoginController extends GetxController {
     VPopup.pop();
     result.when(
       onSuccess: (data) async {
+        await _profileController.getProfile();
         Get.offAllNamed(RouteName.main);
       },
       onFailure: (message) {
